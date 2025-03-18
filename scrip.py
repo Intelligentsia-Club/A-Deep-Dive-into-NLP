5b61eaf5b0e4dd9a865111adb0ed3edf159176ad
Intelligentsia Club <intelligentsia.techclub@gmail.com>
2025-03-18 07:59:31 +0530

    Create scrip.py

---
+++ scrip.py
@@ -0,0 +1,79 @@
+import streamlit as st
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk import pos_tag, DependencyGraph
+import gensim.downloader as api
+
+# nltk.download()
+
+# Load word embeddings (GloVe 50D)
+embeddings = api.load("glove-wiki-gigaword-50")
+
+def Sentence_Segmentation(text):
+    return sent_tokenize(text)
+
+def Word_Tokenization(text):
+    return word_tokenize(text)
+
+def Stemming(words):
+    stemmer = PorterStemmer()
+    return [stemmer.stem(word) for word in words]
+
+def Lemmatization(words):
+    lemmatizer = WordNetLemmatizer()
+    return [lemmatizer.lemmatize(word) for word in words]
+
+def Stop_Word_Analysis(words):
+    stop_words = set(stopwords.words("english"))
+    return [word for word in words if word.lower() not in stop_words]
+
+def POS_Tagging(words):
+    return pos_tag(words)
+
+def Dependency_Parsing(pos_tags):
+    return [(word, tag) for word, tag in pos_tags]
+
+def Word_Embeddings(words):
+    return {word: embeddings[word].tolist() for word in words if word in embeddings}
+
+
+st.title("NLP Pipeline using NLTK")
+text_input = st.text_area("Enter text for processing:")
+
+if st.button("Process Text"):
+    if text_input:
+        ss = Sentence_Segmentation(text_input)
+        st.subheader("Sentence Segmentation")
+        st.write(ss)
+
+        wt = Word_Tokenization(text_input)
+        st.subheader("Word Tokenization")
+        st.write(wt)
+
+        st_words = Stemming(wt)
+        st.subheader("Stemming")
+        st.write(st_words)
+
+        lm_words = Lemmatization(wt)
+        st.subheader("Lemmatization")
+        st.write(lm_words)
+
+        swa = Stop_Word_Analysis(wt) 
+        st.subheader("Stop Word Analysis")
+        st.write(swa)
+
+        pos = POS_Tagging(wt)
+        st.subheader("Part-of-Speech (POS) Tagging")
+        st.write(pos)
+
+        dp = Dependency_Parsing(pos)        
+        st.subheader("Dependency Parsing")
+        st.write(dp)
+        
+        we = Word_Embeddings(wt)                                
+        st.subheader("Word Embeddings")
+        st.write(we)
+    else:
+        st.warning("Please enter some text.")
